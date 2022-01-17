54ec5e61ddb92d45fe8518e01ed18a9e2511a1c1
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-17 21:10:27 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by caro gierich and is open-sourced on{" "}
-        <a
-          href="https://github.com/OlgaMilemis/react-search"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://hardcore-booth-eb65d6.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by caro gierich and is open-sourced on{" "}
+          <a
+            href="https://github.com/OlgaMilemis/react-search"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://hardcore-booth-eb65d6.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
