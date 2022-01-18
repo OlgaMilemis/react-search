c2f9806f306afa654cdaf3358649cbfe70328e81
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-18 13:12:48 +0100

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -9,11 +9,16 @@ export default function Weather() {
           <input
             type="search"
             placeholder="Enter a city"
+            autoFocus="on"
             className="form-control"
           />
         </div>
         <div className="col-3">
-          <input type="submit" value="search" className="btn btn-primary" />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
         </div>
       </form>
       <h1>Berlin</h1>
@@ -21,14 +26,15 @@ export default function Weather() {
         <li>Tuesday 11:35</li>
         <li>Partly Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-5">
         <div className="col-6">
           <img
             src="
           https://www.accuweather.com/images/weathericons/03.svg"
             alt="patly sunny"
           />
-          3° C
+          <span className="temperature">3</span>
+          <span className="unit">° C</span>
         </div>
         <div className="col-6">
           <ul>
