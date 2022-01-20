56ecc93e5c10bab755a2b22c42f9f074152675bb
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-20 16:31:50 +0100

    eliminated Farenheit try

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,54 +1,10 @@
-import React, {useSate} from "react";
+import React from "react";
 
-export default function Temperature(props) {  
-const [temp, setTemp] = useState("celsius");
-
-
-  function displayCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  function displayFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  {if (temp === "celsius")
-return (
-    <div className="Temperature">
-      <span className="temperature">{Math.round(props.data.temperature)}</span>
-      <span className="unit"> <a onClick={celsius}>
-          °C
-        </a>
-        <a className="imp" onClick={fahrenheit}>
-          °F
-        </a></span>
+export default function WeatherTemperature(props) {
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
     </div>
   );
-}
-
-    return (
-      <div>
-        <span id="deg">{Math.round(props.temp)}°C</span>
-        <a className="metric" onClick={celsius}>
-          °C
-        </a>
-        <a className="imp" onClick={fahrenheit}>
-          °F
-        </a>
-      </div>
-    );
-  } else {
-    let fahrenheitTemp = Math.round((props.temp * 9) / 5 + 32);
-    return (
-      <div>
-        <span id="deg">{fahrenheitTemp}°F</span>
-        <button className="butC" onClick={celsius}>
-          °C
-        </button>
-        <button className="butF" onClick={fahrenheit}>
-          °F
-        </button>
-      </div>
-    );
-  }
 }
