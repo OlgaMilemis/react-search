083f9b83a1dedd91d30ecb0682d0ed080f000828
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-24 21:21:16 +0100

    Updated Forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -6,11 +6,13 @@ export default function Forecast(props) {
   function displayForecast(response) {
     console.log(response.data);
   }
+  console.log(props);
+
   let apiKey = "5eac19cf21f53d5d30820a9a9bafd9f0";
   let units = "metric";
   let latitude = props.coordinates.lat;
   let longitude = props.coordinates.lon;
-  let apiUrl = `htpps://https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(displayForecast);
 
@@ -19,10 +21,10 @@ export default function Forecast(props) {
       <div className="row mt-5">
         <div className="col">
           <div className="ForecastDay">Thu</div>
-          <div className="icon">
-            {" "}
-            <img src={props.data.icon} alt={props.data.description} />
-          </div>
+          <img
+            src="http://openweathermap.org/img/wn/04n@2x.png"
+            alt="overcast clouds"
+          />
           <div className="Forecast-temp">
             <span className="Forecast-tempMax">10°</span>
             <span className="Forecast-tempMin">5°</span>
