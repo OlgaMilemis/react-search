e28d011d591331d6b9f1edac0a18b863cbc644ea
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-25 12:06:09 +0100

    tried to fix icon

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 export default function ForecastDay(props) {
-  let icon = `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
   function day() {
+    let icon = `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
+
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
