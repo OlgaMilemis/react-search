57027c0b493332883b43c5786b71edb16d10deb2
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-25 11:29:06 +0100

    adder ForecastDay component

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import ForecastDay from "./ForecastDay";
 import "./Weather.css";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function displayForecast(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  console.log(props);
 
-  let apiKey = "5eac19cf21f53d5d30820a9a9bafd9f0";
-  let units = "metric";
-  let latitude = props.coordinates.lat;
-  let longitude = props.coordinates.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(displayForecast);
-
-  return (
-    <div className="Forecast">
-      <div className="row mt-5">
-        <div className="col">
-          <div className="ForecastDay">Thu</div>
-          <img
-            src="http://openweathermap.org/img/wn/04n@2x.png"
-            alt="overcast clouds"
-          />
-          <div className="Forecast-temp">
-            <span className="Forecast-tempMax">10°</span>
-            <span className="Forecast-tempMin">5°</span>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row mt-5">
+          <div className="col">
+            <ForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "5eac19cf21f53d5d30820a9a9bafd9f0";
+    let units = "metric";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(displayForecast);
+    return null;
+  }
 }
